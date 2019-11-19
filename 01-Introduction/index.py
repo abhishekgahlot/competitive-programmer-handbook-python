e0075c8f23b3d7e016493e257150c44af4f9e02c
Abhishek Gahlot <me@abhishek.it>
2019-11-19 23:21:15 +0530

    Finish first chapter

--- 01-Introduction/index.py
+++ 01-Introduction/index.py
@@ -1,14 +1,16 @@
+import math
+
 # Input -> 10 20 monkey
-#a, b, c = raw_input().split(' ')
-#print(a, b, c)
-#
-## Input with map -> 10 20
-#a, b = map(int, raw_input().split(' '))
-#print(a, b)
-#
-## Input with map -> 10, 20
-#a, b = map(int, raw_input().split(','))
-#print(a, b)
+a, b, c = raw_input().split(' ')
+print(a, b, c)
+
+# Input with map -> 10 20
+a, b = map(int, raw_input().split(' '))
+print(a, b)
+
+# Input with map -> 10, 20
+a, b = map(int, raw_input().split(','))
+print(a, b)
 
 # Python has dynamic data type so integers, float are same as variables
 
@@ -30,5 +32,27 @@ b = set([2, 4])
 print(x)
 print(a.intersection(b))
 print(a.union(b))
+print(a.difference(b))
 
+# Functions
+print(3/2)
+print(min([1, 2, 3]))
+print(max([1, 2, 3]))
+print(math.factorial(10))
 
+# Not memoized
+def fibo(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fibo(n-1) + fibo(n-2)
+
+print(fibo(10))
+
+# Logarithms
+## logk(a, b) = logk(a) + logk(b)
+## logk(x^n) = n * logk(x)
+## logk(a/b) = logk(a) - logk(b)
+## logu(x) = logk(x) / logk(u)
+print(math.log(32, 2))
