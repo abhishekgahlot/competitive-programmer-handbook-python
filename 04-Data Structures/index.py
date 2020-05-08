5de189267e6f4e9d588461e150129665cd6c7520
Abhishek Gahlot <me@abhishek.it>
2020-05-08 15:12:04 +0530

    Move print to function

--- 04-Data Structures/index.py
+++ 04-Data Structures/index.py
@@ -4,36 +4,36 @@
 arr = [1, 2, 3, 4, 5, 6]
 arr.pop()
 arr.pop()
-print arr
+print(arr)
 
 arr.append(1)
-print arr
+print(arr)
 
 # Set
 s = set([1, 2, 3, 4, 5, 2])
-print s
+print(s)
 
 s.remove(5)
-print s
+print(s)
 
 s.add(6)
-print s
+print(s)
 
-print len(s)
+print(len(s))
 
 # Map or Object
 dictionary = {}
 dictionary[1] = 2
 dictionary[2] = 3
 
-print dictionary
-print dictionary.keys()
-print dictionary.values()
-print dictionary.items()
+print(dictionary)
+print(dictionary.keys())
+print(dictionary.values())
+print(dictionary.items())
 
 for i,j in dictionary.items():
-	print i,j
+	print(i,j)
 
-print 2 in dictionary
-print 3 in dictionary
+print(2 in dictionary)
+print(3 in dictionary)
 
